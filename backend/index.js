c8dc1ef0ce124d1b01f91482172d60c52abefcfb
Himanshu kumar <Himanshu@vgu.ac.in>
2026-04-28 13:17:40 +0530

    final backend fix

--- backend/index.js
+++ backend/index.js
@@ -18,7 +18,8 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 const corsOptions = {
-    origin: "http://localhost:5173",
+    // origin: "http://localhost:5173",
+    origin:"https://job-portal-red-beta.vercel.app",
     credentials: true
 }
 app.use(cors(corsOptions));
